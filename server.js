9ef9d1b7ab3aa42b4c0c16e6c30661c0cccf497e
Clarissa Mobley <clarissamobley@me.com>
2024-06-03 15:50:27 -0400

    Added express, nodemon, handlebars, and a starting index page

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up static files
+app.use(express.static("public"));
+
+// Set up Handlebars engine
+const hbs = exphbs.create({
+  extname: "hbs",
+  layoutsDir: `${__dirname}/views/layouts`,
+  defaultLayout: "index",
+  partialsDir: `${__dirname}/views/partials`,
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+
+
+// Route for handlebars
+app.get("/", (req, res) => {
+  res.render("main", { layout: "index" });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});
