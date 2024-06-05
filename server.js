b4cad7ebcbc76e1928ee8e05935f40afb6bb5bb3
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-04 22:24:15 -0500

    server.js updates
    added runtimeOptions in handlebars engine setup to allow for use of prototypes without errors from database.
    
    setup body parser middleware for future use

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+const express = require("express"); // Express module
+const exphbs = require("express-handlebars"); // Handlebars module
 const path = require("path"); // Path module
 const bodyParser = require("body-parser"); // Body parser module
 
-
-
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // Initialize express
+const PORT = process.env.PORT || 3001; // Port number
 
 //database require
 const db = require('./config/database')
@@ -14,7 +12,7 @@ const db = require('./config/database')
 //test database
 db.authenticate()
   .then(() => console.log('Database connected...'))
-  .catch(err => console.log('Error: ' + err))
+  .catch(err => console.log('Error: Database unable to connect.... ' + err))
 
 // Set up static files
 app.use(express.static("public"));
@@ -25,10 +23,17 @@ const hbs = exphbs.create({
   layoutsDir: `${__dirname}/views/layouts`,
   defaultLayout: "index",
   partialsDir: `${__dirname}/views/partials`,
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true, // Disables warning message allows for the use of the prototype
+    allowProtoMethodsByDefault: true,  // Disables warning message allows for the use of the prototype
+  }
 });
 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
+
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Route for handlebars
