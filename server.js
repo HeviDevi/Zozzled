3da1e856e30dc4eac7efee675b4d8cff191a654e
Clarissa Mobley <clarissamobley@me.com>
2024-06-05 12:58:25 -0400

    Created drink details page and container for the drink info

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ app.get("/drink-results", (req, res) => {
   res.render("drink-results", { layout: "index" })
 });
 
+// Route for drink-details
+app.get("/drink-details", (req, res) => {
+  res.render("drink-details", { layout: "index" })
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`App listening to port ${PORT}`);
