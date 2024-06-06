6715a8fa12c7c7014e7aab2bae400e8f6cfa5ad7
Clarissa Mobley <clarissamobley@me.com>
2024-06-06 18:54:37 -0400

    testing drink card

--- models/User.js
+++ models/User.js
@@ -1,22 +1,66 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+// const Sequelize = require('sequelize');
+// const db = require('../config/database');
 
-const User = db.define('userdrinks', {
-    drinkname: {
-        type: Sequelize.STRING
-    }, 
-    spirittype: {
-        type: Sequelize.STRING
+// const User = db.define('userdrinks', {
+//     drinkname: {
+//         type: Sequelize.STRING
+//     }, 
+//     spirittype: {
+//         type: Sequelize.STRING
+//     },
+//     spiritamount: {
+//         type: Sequelize.STRING
+//     },
+//     ingredients: {
+//         type: Sequelize.STRING
+//     },
+//     instructions: {
+//         type: Sequelize.STRING
+//     }
+// });
+
+// module.exports = User;
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class UserDrink extends Model {}
+
+UserDrink.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        drinkname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        spirittype: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        spiritamount: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ingredients: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
     },
-    spiritamount: {
-        type: Sequelize.STRING
-    },
-    ingredients: {
-        type: Sequelize.STRING
-    },
-    instructions: {
-        type: Sequelize.STRING
+    {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'userdrink',
     }
-});
+);
 
-module.exports = User;
+module.exports = UserDrink;
