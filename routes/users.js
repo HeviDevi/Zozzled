a771125871b5a3baef9581b548775978e2325277
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-06 23:06:43 -0500

    Drink search updates
    drink-results.hbs: updated context in hbs file to allow for correct rendering of drink results
    
    users.js: added logging to users.js for drink route , also added correct route to search options

--- routes/users.js
+++ routes/users.js
@@ -54,25 +54,24 @@ router.post('/add', async (req, res) => {
     }  
 });
 
-//Search for a drink route
+// Search for a drink route
 router.get('/search', async (req, res) => {
     const { term } = req.query;
-    console.log('Search term:', term); // Log the search term
+
+    console.log("Search term:", term); // Log the search term
 
     try {
-        const drinks = await Drinks.findAll({ 
-            where: { 
-                drink_name: { 
-                    [Op.like]: '%' + term + '%' 
-                } 
-            } 
-        });
+        const drinks = await Drinks.findAll({ where: { drink_name: { [Op.like]: '%' + term + '%' } } });
         console.log("Found drinks:", drinks); // Log the retrieved drinks
-        res.render('drinks', { drinks }); // Render the 'drinks' view with the drinks data
+        
+        if (drinks.length === 0) {
+            console.log("No drinks found for term:", term);
+        }
+
+        res.render('drink-results', { drinks }); // Rendering the 'drink-results' view with the drinks data
     } catch (err) {
         console.error("Database error:", err.message); // Log the database error message
         res.status(500).send('Server Error');
     }
 });
-
 module.exports = router; // Export the router
