d302e39cdc8a64e9e458703a3bc321c4efaf22c9
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-06 20:12:03 -0500

    database changes
    updated drinkrecipes table with new columns for revised criteria.

--- models/User.js
+++ models/User.js
@@ -1,26 +1,3 @@
-// const Sequelize = require('sequelize');
-// const db = require('../config/database');
-
-// const User = db.define('userdrinks', {
-//     drinkname: {
-//         type: Sequelize.STRING
-//     }, 
-//     spirittype: {
-//         type: Sequelize.STRING
-//     },
-//     spiritamount: {
-//         type: Sequelize.STRING
-//     },
-//     ingredients: {
-//         type: Sequelize.STRING
-//     },
-//     instructions: {
-//         type: Sequelize.STRING
-//     }
-// });
-
-// module.exports = User;
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
