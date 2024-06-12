80a69565aae0efdb931a68aecb0a8ae03225b4ac
Alexander Happel <Alex.happel90@gmail.com>
2024-06-12 13:02:44 -0400

    forgot to move Zach's stuff back

--- server.js
+++ server.js
@@ -1,54 +1,3 @@
-// //stuff Eddie tested out with Ryan
-// // const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// // const sess = {
-// //   secret: 'Super secret secret',
-// //   cookie: {
-// //     maxAge: 300000,
-// //     httpOnly: true,
-// //     secure: false,
-// //     sameSite: 'strict',
-// //   },
-// //   resave: false,
-// //   saveUninitialized: true,
-// //   store: new SequelizeStore({
-// //     db: sequelize
-// //   })
-// // };
-// // app.use(session(sess));
-
-// // I am consolidating my code here to make it easier to read and understand. DO NOT MOVE OR MODIFY
-// // ANYTHING BELOW THIS LINE. - Zachary Testing
-
-// //Bring database connection
-// const db = require("./config/database");
-
-// //Test database connection
-// db.sync()
-//   .then(() => console.log("Database connected..."))
-//   .catch((err) => console.log("Error: " + err));
-
-// // Set up static files
-// app.use(express.static("public"));
-
-// // Set up body parser
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // Body parser middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // User routes - anything that is /users will go to the users.js file
-// app.use("/users", require("./routes/users"));
-
-// // Search routes - anything that is /search will go to the search.js file
-// app.use('/', require('./routes/search'));
-
-// //Drink Detail
-// app.use('/', require('./routes/drinks'));
-
-
-// //END OF CODE CONSOLIDATION - Zachary
-
 require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -67,21 +16,38 @@ const Login = require('./models/login');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Test database connection
-sequelize.authenticate()
-  .then(() => {
-    console.log("Database connected...");
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log("Database synchronized...");
-  })
-  .catch((err) => {
-    console.log("Error connecting to the database: " + err);
-  });
+
+// //stuff Eddie tested out with Ryan
+// // const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// // const sess = {
+// //   secret: 'Super secret secret',
+// //   cookie: {
+// //     maxAge: 300000,
+// //     httpOnly: true,
+// //     secure: false,
+// //     sameSite: 'strict',
+// //   },
+// //   resave: false,
+// //   saveUninitialized: true,
+// //   store: new SequelizeStore({
+// //     db: sequelize
+// //   })
+// // };
+// // app.use(session(sess));
+
+// I am consolidating my code here to make it easier to read and understand. DO NOT MOVE OR MODIFY
+// ANYTHING BELOW THIS LINE. - Zachary Testing
+
+//Bring database connection
+const db = require("./config/database");
+
+//Test database connection
+db.sync()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log("Error: " + err));
 
 // Set up static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Set up body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -90,14 +56,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// User routes
-app.use('/users', require('./routes/users'));
+// User routes - anything that is /users will go to the users.js file
+app.use("/users", require("./routes/users"));
 
-// Search routes
+// Search routes - anything that is /search will go to the search.js file
 app.use('/', require('./routes/search'));
 
-// Drink Detail routes
+//Drink Detail
 app.use('/', require('./routes/drinks'));
+
+//END OF CODE CONSOLIDATION - Zachary
 
 // Set up Handlebars engine
 const hbs = exphbs.create({
