62f63179a42c945a490bf0bbf3d870898fb888d5
Clarissa Mobley <clarissamobley@me.com>
2024-06-13 21:50:33 -0400

    changed to strict

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.use(
     cookie: {
       secure: false,
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000 // 1 day in milliseconds
     }
   })
