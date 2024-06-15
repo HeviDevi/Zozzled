5150cd31d67475c67b896e2ef829cbb76be1a62c
Zachary Polof <ZacharyPolof@gmail.com>
2024-06-15 16:26:30 -0500

    drink-details
    added drink details to twitter, to try and render drink information into twitter post

--- public/js/socials.js
+++ public/js/socials.js
@@ -1,17 +1,21 @@
 //tweet function
-function tweet() {
-    window.open("https://twitter.com/intent/tweet?text=" ,"Tweet Window", "width=600, height=300");
+function tweet(message) {
+    const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
+    window.open(url, "Tweet Window", "width=600, height=300");
 }
-
 //instagram function
 function instagram() {
     window.open("https://www.instagram.com/", "Instagram Window", "width=600, height=300");
 }
 
+// Construct drink details message for the tweet
+const drinkDetails = `Check out this amazing drink! Name: ${'{{name}}'}, Spirit: ${'{{spirit}}'}, Ingredients: ${'{{ingredients}'}`;
+
 //on click of tweet button
-document.getElementById('twitter',).addEventListener('click', () => {
-    tweet();
+document.getElementById('twitter').addEventListener('click', () => {
+    tweet(drinkDetails);
 });
+
 
 
 //on click of instagram button
