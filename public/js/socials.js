0983303fbb9041f760f4209b43c6b0a48525e623
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-06-15 14:49:40 -0500

    Update socials.js
    added functions for tweet and instagram

---
+++ public/js/socials.js
@@ -0,0 +1,15 @@
+//tweet function
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ---- By " + 
+    author.innerHTML, "Tweet Window", "width=600, height=300");
+}
+
+//instagram function
+function instagram() {
+    window.open("https://www.instagram.com/", "Instagram Window", "width=600, height=300");
+}
+
+//on click of tweet button
+
+
+//on click of instagram button
